31527679ddeb0cf8f158a365dde081e3cff18903
tabishadnan <tabishadnan000@gmail.com>
2020-12-23 22:16:09 +0500

    Updates

---
+++ precache-manifest.8d233fe1b96a74c7e252385cdef68167.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da2c5a02eb60fece66e6a8001c936677",
+    "url": "/color-box-exercise/index.html"
+  },
+  {
+    "revision": "b24c6adc76b8f6ba98d3",
+    "url": "/color-box-exercise/static/css/main.a41f23ae.chunk.css"
+  },
+  {
+    "revision": "4a21d5a4eab7f603792a",
+    "url": "/color-box-exercise/static/js/2.231f006d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/color-box-exercise/static/js/2.231f006d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b24c6adc76b8f6ba98d3",
+    "url": "/color-box-exercise/static/js/main.e693a115.chunk.js"
+  },
+  {
+    "revision": "2ae3b4ceed28ba0f2778",
+    "url": "/color-box-exercise/static/js/runtime-main.eae0dd29.js"
+  }
+]);
